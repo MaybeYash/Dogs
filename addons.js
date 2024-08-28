59407327c914ead9c78c7a402a838e01eef4ba3a
Yᴀsʜ <unknownsoumo@gmail.com>
2024-08-28 09:01:42 +0530

    ig

--- addons.js
+++ addons.js
@@ -4,18 +4,18 @@ $(document).ready(function() {
 });
 
 function updateCryptoData() {
-    $.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,TON,DOGS,NOT&tsyms=USD")
+    $.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,TON,NOT,DOGS&tsyms=USDT")
         .done(function(data) {
             updateCoinData(data, "BTC", "#btcLogo", "#btcSymbol", "#btcPrice", "#btcChange", "#btcVol", "#btcCap", "#btcTime");
             updateCoinData(data, "ETH", "#ethLogo", "#ethSymbol", "#ethPrice", "#ethChange", "#ethVol", "#ethCap");
             updateCoinData(data, "TON", "#tonLogo", "#tonSymbol", "#tonPrice", "#tonChange", "#tonVol", "#tonCap");
-            updateCoinData(data, "DOGS", "#dogsLogo", "#dogsSymbol", "#dogsPrice", "#dogsChange", "#dogsVol", "#dogsCap");
             updateCoinData(data, "NOT", "#notLogo", "#notSymbol", "#notPrice", "#notChange", "#notVol", "#notCap");
+            updateCoinData(data, "DOGS", "#dogsLogo", "#dogsSymbol", "#dogsPrice", "#dogsChange", "#dogsVol", "#dogsCap");
         });
 }
 
 function updateCoinData(data, coin, logoSelector, symbolSelector, priceSelector, changeSelector, volumeSelector, capSelector, timeSelector = null) {
-    const coinData = data.RAW[coin].USD;
+    const coinData = data.RAW[coin].USDT;
     
     $(logoSelector).html('<img src="https://cryptocompare.com' + coinData.IMAGEURL + '" alt="' + coin + ' Logo">');
     $(symbolSelector).text(coinData.FROMSYMBOL);
